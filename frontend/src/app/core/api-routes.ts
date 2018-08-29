df18d6a43c1a8a77de8e7a320547d79f0437058a
Andrei Babkin <t091342@gmail.com>
2018-08-29 21:05:35 +0300

    Moves API path origin definition into the environments

--- frontend/src/app/core/api-routes.ts
+++ frontend/src/app/core/api-routes.ts
@@ -1,12 +1,10 @@
-//const API_ORIGIN = 'http://localhost/mlden/htdocs/api/'; // dev
-const API_ORIGIN = 'http://malachiteden.local/api/'; // dev 2
-//const API_ORIGIN = 'http://malachiteden.com/api/'; // prod
+import { environment } from "../../environments/environment";
 
 /** Get object with text strings used on the site. */
-export const API_CONTENT = API_ORIGIN + 'content';
+export const API_CONTENT = environment.apiOrigin + 'content';
 
 /** Send a message from site visitor */
-export const API_SEND_MSG = API_ORIGIN + 'contact';
+export const API_SEND_MSG = environment.apiOrigin + 'contact';
 
 /** Get a list of completed projects (array) */
-export const API_PORTFOLIO = API_ORIGIN + 'portfolio';
+export const API_PORTFOLIO = environment.apiOrigin + 'portfolio';
