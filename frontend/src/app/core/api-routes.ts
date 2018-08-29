b842dabe79ee1be96ef98a057533d0adbe67872c
Andrei Babkin <t091342@gmail.com>
2018-08-29 21:29:25 +0300

    Changes paths

--- frontend/src/app/core/api-routes.ts
+++ frontend/src/app/core/api-routes.ts
@@ -1,10 +1,12 @@
 import { environment } from "../../environments/environment";
 
+const default_prefix = environment.apiOrigin + 'api/';
+
 /** Get object with text strings used on the site. */
-export const API_CONTENT = environment.apiOrigin + 'api/content';
+export const API_CONTENT = default_prefix + 'content';
 
 /** Send a message from site visitor */
-export const API_SEND_MSG = environment.apiOrigin + 'api/contact';
+export const API_SEND_MSG = default_prefix + 'contact';
 
 /** Get a list of completed projects (array) */
-export const API_PORTFOLIO = environment.apiOrigin + 'api/portfolio';
+export const API_PORTFOLIO = default_prefix + 'portfolio';
