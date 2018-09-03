6605191b61efb65c1920cb560cb92ae9f53dcf58
Andrei Babkin <t091342@gmail.com>
2018-09-03 17:39:07 +0300

    Changes CORS handle to add headers only when they are needed.

--- app/Http/Middleware/AllowCORS.php
+++ app/Http/Middleware/AllowCORS.php
@@ -15,8 +15,21 @@ class AllowCORS
      */
     public function handle($request, Closure $next)
     {
+      if($this->isCorsRequest($request)){
         header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-        return $next($request);
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, UPDATE, PATCH, PUT');
+        header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+      }
+      return $next($request);
+    }
+
+    private function isCorsRequest($request)
+    {
+      return $request->headers->has('Origin') && !$this->isSameHost($request);
+    }
+
+    private function isSameHost($request)
+    {
+      return $request->headers->get('Origin') === $request->getSchemeAndHttpHost();
     }
 }
